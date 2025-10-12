8f5ed3febe4094001b904590f664c83e7f6ec47b
Ashlar <luke.boustridge@gmail.com>
2025-10-12 20:17:17 +1300

    fix: remove invalid vercel runtime override

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,29 @@
+import { Role } from "@prisma/client";
+import "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string | null;
+      email: string;
+      role: Role;
+    };
+  }
+
+  interface User {
+    role: Role;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: Role;
+  }
+}
+
+declare module "next-auth/adapters" {
+  interface AdapterUser {
+    role: Role;
+  }
+}
