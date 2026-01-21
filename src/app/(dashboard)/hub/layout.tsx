e31155f4bdc154ace060b297f7a3f1e43b6d41ec
Luke Boustridge <luke.boustridge@gmail.com>
2026-01-22 08:54:06 +1300

    Allow SCM access to hub

--- src/app/(dashboard)/hub/layout.tsx
+++ src/app/(dashboard)/hub/layout.tsx
@@ -16,7 +16,7 @@ export default async function HubLayout({
   }
 
   const canAccessHub =
-    user.isAdmin || user.role === Role.SA || user.role === Role.Secretariat;
+    user.isAdmin || user.role === Role.SA || user.role === Role.SCM || user.role === Role.Secretariat;
 
   if (!canAccessHub) {
     redirect("/dashboard");
